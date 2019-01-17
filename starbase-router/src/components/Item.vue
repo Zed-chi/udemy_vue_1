0f9aa972bcb5482dc6924e01ab69d9d709744b74
AlexE <zed-chi@mail.ru>
2019-01-17 22:46:02 +0300

    added item component

---
+++ starbase-router/src/components/Item.vue
@@ -0,0 +1,25 @@
+<template>
+        <div class="col-md-4">
+            <div class="item-card">
+                <div class="card-block">
+                    <div class="card-title">
+                       <h4>{{item.name}} </h4>
+                       <div v-for="(value, key, index) in items">
+                           <div v-if="index < 5">
+                               <strong>
+                                   {{key}}
+                               </strong>:{{value}}
+                           </div>
+                       </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+    props:['item']
+}
+</script>
+
