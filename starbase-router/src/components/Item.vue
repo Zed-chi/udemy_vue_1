96097b710ed5baa5c274a16cece1c422fbc13690
AlexE <zed-chi@mail.ru>
2019-01-17 23:10:57 +0300

    added switch item method

--- starbase-router/src/components/Item.vue
+++ starbase-router/src/components/Item.vue
@@ -1,25 +1,41 @@
 <template>
-        <div class="col-md-4">
-            <div class="item-card">
-                <div class="card-block">
-                    <div class="card-title">
-                       <h4>{{item.name}} </h4>
-                       <div v-for="(value, key, index) in items">
-                           <div v-if="index < 5">
-                               <strong>
-                                   {{key}}
-                               </strong>:{{value}}
-                           </div>
-                       </div>
+    <div class="col-md-4" @click="switchItem">
+        <div class="item-card">
+            <div class="card-block">
+                <h4>{{item.name}}</h4>
+
+                <div v-for="(value, key, index) in item">
+                    <div v-if="index < 5">
+                        <strong>{{key}}</strong>:{{value}}
                     </div>
                 </div>
+
             </div>
         </div>
+    </div>
 </template>
 
 <script>
 export default {
-    props:['item']
+    props:['passedItem', "type"],
+    data(){
+        return {
+            item: {}
+        }
+    },
+    methods:{
+        switchItem(){
+            let random_id = Math.floor(Math.random()*63);
+            fetch(
+                `https://swapi.co/api/${this.type}/${random_id}`, 
+                {method:"GET"})
+            .then(response => response.json())
+            .then(json => this.item = json)
+        }
+    },
+    created(){
+        this.item = this.passedItem;
+    }
 }
 </script>
 
