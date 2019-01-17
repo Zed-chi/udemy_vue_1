034d86caaaa52d885faa8b3524b0e2a8d3e156aa
AlexE <zed-chi@mail.ru>
2019-01-17 22:35:37 +0300

    changed component content

--- starbase-router/src/components/Data.vue
+++ starbase-router/src/components/Data.vue
@@ -1,21 +1,37 @@
 <template>
     <div>
-        Type: {{type}}
+        {{items}}
     </div>
 </template>
 
 <script>
     export default {
         data(){
-            return {type: this.$route.params.type}
+            return {
+                type: this.$route.params.type,
+                items: []
+            }
         },
         watch: {
-            "$route":"change"
+            "$route":"fetchItems"
         },
         methods:{
-            change(){
-                this.type = this.$route.params.type;
+            fetchItems(){
+                this.items = [];
+                let init_ids = [2,4,7];
+                for (let i in init_ids) {
+                    let id = init_ids[i];
+                    console.log("id", id)
+                    fetch(
+                        `https://swapi.co/api/${this.type}/${id}`, 
+                        {method:"GET"})
+                    .then(response => response.json())
+                    .then(json => this.items.push(json))
+                }
             }
+        },
+        created(){
+            this.fetchItems()
         }
     };
 </script>
