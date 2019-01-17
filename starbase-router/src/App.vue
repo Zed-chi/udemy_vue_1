a5a29c60cad2cbda013411546e1687f44ede00a2
AlexE <zed-chi@mail.ru>
2019-01-17 22:22:40 +0300

    sr-router

---
+++ starbase-router/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <h3>Star something router</h3>
+    <h4>
+      <router-link class="link" to="/data/people">Meets guys</router-link>
+      |
+      <router-link class="link" to="/data/planets">Meet planets</router-link>
+    </h4>
+    <router-view></router-view>
+  </div>
+</template>
---
+++ starbase-router/src/components/Data.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        Type: {{type}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {type: this.$route.params.type}
+        },
+        watch: {
+            "$route":"change"
+        },
+        methods:{
+            change(){
+                this.type = this.$route.params.type;
+            }
+        }
+    };
+</script>
